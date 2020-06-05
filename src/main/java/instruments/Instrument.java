11a0d755545a211b51cc987d82761b6324021476
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-05 21:30:54 +0100

    adds Instrument abstract class, with constructor and getters and setters, to be tested

---
+++ src/main/java/instruments/Instrument.java
@@ -0,0 +1,91 @@
+package instruments;
+
+import enums_categories.InstrumentType;
+import enums_categories.Material;
+
+public abstract class Instrument {
+
+    private String name;
+    private String brand;
+    private Material material;
+    private String colour;
+    private InstrumentType type;
+    private boolean electric;
+    private double buyPrice;
+    private double sellPrice;
+
+    public Instrument(String name, String brand, Material material, String colour, InstrumentType type, boolean electric, double buyPrice, double sellPrice) {
+        this.name = name;
+        this.brand = brand;
+        this.material = material;
+        this.colour = colour;
+        this.type = type;
+        this.electric = electric;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public InstrumentType getType() {
+        return type;
+    }
+
+    public void setType(InstrumentType type) {
+        this.type = type;
+    }
+
+    public boolean isElectric() {
+        return electric;
+    }
+
+    public void setElectric(boolean electric) {
+        this.electric = electric;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+}
