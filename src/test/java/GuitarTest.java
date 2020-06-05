9bf9060d53ee0f2123866787186f858c68cb020e
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-05 21:40:16 +0100

    adds getName and getBrand tests

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,5 +1,37 @@
+import enums_categories.InstrumentType;
+import enums_categories.Material;
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
 
     private Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("telecaster",
+                "fender",
+                Material.WOOD,
+                "sunburst orange",
+                InstrumentType.STRINGS,
+                true,
+                60,
+                120,
+                6);
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("telecaster", guitar.getName());
+    }
+
+    @Test
+    public void canGetBrand(){
+        assertEquals("fender", guitar.getBrand());
+    }
 
 }
